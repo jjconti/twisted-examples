e5c4876a0dcc6913b9fbd384391477bf31155901
jjconti <jjconti@c17424ab-9666-0410-ad2c-99304602335c>
2009-04-02 20:31:03 +0000

    Un servidor listo para mostrar por primera vez en Teco.

--- teco/line.py
+++ teco/line.py
@@ -1,5 +1,5 @@
 from twisted.protocols.basic import LineOnlyReceiver
-
+from twisted.application import internet, service
 from twisted.internet.protocol import Factory
 from twisted.internet import reactor
 
@@ -11,14 +11,27 @@ from sys import stdout
 class TModBus(LineOnlyReceiver):
 
     def lineReceived(self, line):
-        print 'R: ' + line
+        print 'Recived: ' + line
+        self.process(line)
         
     def connectionMade(self):
-        self.transport.write(":CONECTADO\r\n") 
+        self.peer = self.transport.getPeer()
+        self.transport.write("Bienvenido %s:%d" % (self.peer.host, self.peer.port) + '\r\n')
+
+    def process(self, line):    #FIXME: esta funcion debe detectar errores
+        print '-' * 80
+        print "Mensaje desde %s:%d" % (self.peer.host, self.peer.port) 
+        if not line.startswith(':'):
+            print "Error en mensaje: no empieza con :"
+        else:
+            dispid = line[1:3]
+            print "ID de dispositivo: " + dispid
+            funcode = line[3:5]
+            print "Function Code: " + funcode
+        print '-' * 80
         
 class TModBusFactory(Factory):
     protocol = TModBus
 
 reactor.listenTCP(8007, TModBusFactory())
 reactor.run()
-
